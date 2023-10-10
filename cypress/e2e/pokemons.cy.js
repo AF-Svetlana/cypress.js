d1e791d91d6b06b2cb8acf31168dd991b822d5e5
Svetlana Afonina <142018455+AF-Svetlana@users.noreply.github.com>
2023-10-10 12:14:40 +0300

    Update pokemons.cy.js

--- cypress/e2e/pokemons.cy.js
+++ cypress/e2e/pokemons.cy.js
@@ -1,8 +1,8 @@
 describe('Тестирование покупки нового скина на https://pokemonbattle.me', function () {
    it('Проверка, что при вводе правильного логина и пароля происходит авторизация', function () {
         cy.visit('https://pokemonbattle.me/login');
-        cy.get(':nth-child(1) > .auth__input').type('ms.gerus@yandex.ru'); //Впишите свой логин!
-        cy.get('#password').type('A1702s93'); //Впишите свой пароль!
+        cy.get(':nth-child(1) > .auth__input').type('@yandex.ru'); //Впишите свой логин!
+        cy.get('#password').type('*****'); //Впишите свой пароль!
         cy.get('.auth__button').click();
         cy.get('.header__btns > [href="/shop"]').click();
         cy.get('.shop__list > li').not('.feature-empty').children('.shop__button').eq(0).click(); //Сложный селектор, выбирает первый из скинов, доступных к покупке, исключая уже текущий скин
